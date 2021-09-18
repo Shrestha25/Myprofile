28d581dccd0995b78cf6cc68ea18c92fabfd1658
Shrestha25 <25shrestha97@gmail.com>
2021-09-18 23:45:43 +0530

    Updates

---
+++ precache-manifest.705f8fcb95613d2647877bf22d67e226.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b2664494dc65883cdd6",
+    "url": "/Myprofile/static/css/main.efed5a7d.chunk.css"
+  },
+  {
+    "revision": "9b2664494dc65883cdd6",
+    "url": "/Myprofile/static/js/main.9b266449.chunk.js"
+  },
+  {
+    "revision": "e40f710638f175e2677c",
+    "url": "/Myprofile/static/js/1.e40f7106.chunk.js"
+  },
+  {
+    "revision": "f65441c89b5ede2d8190",
+    "url": "/Myprofile/static/js/runtime~main.f65441c8.js"
+  },
+  {
+    "revision": "d950ad8575823184a642f42dbee4b18d",
+    "url": "/Myprofile/static/media/logo.d950ad85.png"
+  },
+  {
+    "revision": "12eb71091df2d09723a1fe17b1ff4d96",
+    "url": "/Myprofile/static/media/my.12eb7109.jpeg"
+  },
+  {
+    "revision": "b6798bd4086f15cbb86d1876bf8130d2",
+    "url": "/Myprofile/static/media/codechef.b6798bd4.jpg"
+  },
+  {
+    "revision": "1e07687bf8c0bfe9ff477063911841df",
+    "url": "/Myprofile/static/media/stool_logo.1e07687b.png"
+  },
+  {
+    "revision": "59d7b283900f6520071e3ca1e9ef0c75",
+    "url": "/Myprofile/static/media/WampServer.59d7b283.png"
+  },
+  {
+    "revision": "b596cdeb5f1f86a00f56b00f6cb13e93",
+    "url": "/Myprofile/static/media/nsssymbol.b596cdeb.jpeg"
+  },
+  {
+    "revision": "db2acd4635d523644f93278c20174740",
+    "url": "/Myprofile/static/media/jagrati.db2acd46.png"
+  },
+  {
+    "revision": "39abaf6b27cfa76e8c6a0f2d1467444a",
+    "url": "/Myprofile/static/media/face.39abaf6b.jpg"
+  },
+  {
+    "revision": "8404c2a0c4a4eedd0f38ee5995ccc182",
+    "url": "/Myprofile/static/media/insta.8404c2a0.png"
+  },
+  {
+    "revision": "72d7fe9ce4f351c2622ceba2413fa25c",
+    "url": "/Myprofile/static/media/mail.72d7fe9c.png"
+  },
+  {
+    "revision": "cc3a0377ce14eb85cc8d928e602fb406",
+    "url": "/Myprofile/static/media/github.cc3a0377.png"
+  },
+  {
+    "revision": "d32c50d85f0ea620e991250cf2e8cf45",
+    "url": "/Myprofile/index.html"
+  }
+];
